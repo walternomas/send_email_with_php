242bb072c4fa6b67e38acdbd52b00b49eb48f6e0
walternomas <w.barriosvazquez@gmail.com>
2022-05-12 02:49:41 -0300

    language change - Spanish for English

--- index.php
+++ index.php
@@ -14,9 +14,9 @@ if ( isset($_POST["form"]) ) {
         $subject = $_POST["subject"];
         $message = $_POST["message"];
 
-        $body = "$name <$email> te envía el siguiente mensaje: <br><br> $message";
+        $body = "$name <$email> sends you the following message: <br><br> $message";
 
-        // Mandar el correo
+        // Send the mail
         sendMail($subject, $body, $email, $name, true);
 
         $status = "success";
@@ -26,54 +26,53 @@ if ( isset($_POST["form"]) ) {
 }
 ?>
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Formulario de contacto</title>
+    <title>Contact Form</title>
 </head>
 <body>
 
     <?php if ($status == "danger"): ?>
     <div class="alert danger">
-        <span>Surgió un problema</span>
+        <span>There was a problem</span>
     </div>
     <?php endif; ?>
 
     <?php if ($status == "success"): ?>
     <div class="alert success">
-        <span>¡Mensaje enviado con éxito!</span>
+        <span>Message sent succesfully!</span>
     </div>
     <?php endif; ?>
 
     <form action="./" method="POST">
 
-        <h1>¡Contáctanos!</h1>
+        <h1>Contact us!</h1>
 
         <div class="input-group">
-            <label for="name">Nombre:</label>
+            <label for="name">Name:</label>
             <input type="text" name="name" id="name">
         </div>
 
         <div class="input-group">
-            <label for="email">Correo:</label>
+            <label for="email">Mail:</label>
             <input type="email" name="email" id="email">
         </div>
 
         <div class="input-group">
-            <label for="subject">Asunto:</label>
+            <label for="subject">Subject:</label>
             <input type="text" name="subject" id="subject">
         </div>
 
         <div class="input-group">
-            <label for="message">Mensaje:</label>
+            <label for="message">Message:</label>
             <textarea name="message" id="message"></textarea>
         </div>
 
         <div class="button-container">
-            <button name="form" type="submit">Enviar</button>
+            <button name="form" type="submit">Send</button>
         </div>
 
         <div class="contact-info">
